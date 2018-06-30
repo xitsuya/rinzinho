45072869c8bdcddee960bf2104f35d4cd631176f
Glitch (scarce-cocktail) <none>
2018-06-30 02:31:54 +0000

    🐽🔋 Checkpoint ./commands/kick.js:1323575/5663

--- commands/kick.js
+++ commands/kick.js
@@ -2,10 +2,6 @@ const Discord = require("discord.js")
 
 exports.run = (bot, message, args) => {
 
-​
-
-​
-
     if (!message.member.hasPermission("KICK_MEMBERS")) return;
 
     let guild = message.guild;
@@ -17,8 +13,6 @@ exports.run = (bot, message, args) => {
     let user = message.mentions.users.first();
 
     let channel = guild.channels.find("name", "ban-log");
-
-​
 
         var embed = new Discord.RichEmbed()
 
@@ -34,9 +28,6 @@ exports.run = (bot, message, args) => {
 
        if (message.mentions.users.size < 1) return message.channel.send({ embed }).then(message => message.delete(5000));
 
-​
-
-​
 
     var embed = new Discord.RichEmbed()
 
@@ -44,15 +35,13 @@ exports.run = (bot, message, args) => {
 
         .setTimestamp()
 
-        .addField("Você precisa dizer um motivo para esse kick!", "`+kick @user reason`")
+        .addField("Você precisa dizer um motivo para esse kick!", "+kick @user reason")
 
         .setFooter("Rin-Bot", "https://cdn.discordapp.com/avatars/366922505480568852/8bc60c31c633300d19976f5a353784a6.png?size=2048")
 
         .setColor("#91489a")
 
     if (reason.length < 1) return message.channel.send({ embed }).then(message => message.delete(5000));
-
-​
     var embed = new Discord.RichEmbed()
         .setAuthor("| Erro!", message.author.avatarURL)
         .setTimestamp()
@@ -66,10 +55,8 @@ exports.run = (bot, message, args) => {
     var embed = new Discord.RichEmbed()
         .setAuthor("Kick | " + user.tag)
         .setTimestamp()
-        .addField('Membro:', `${user.username}#` + user.discriminator, true)
-        .addField('Staff:', `${message.author.username}`, true)
+        .addField('Membro:', user.username+ user.discriminator, true)
+        .addField('Membro:', member, true)
         .addField('Motivo:', reason, true)
         .setFooter("Rin-Bot", "https://cdn.discordapp.com/avatars/366922505480568852/8bc60c31c633300d19976f5a353784a6.png?size=2048")
             channel.send({ embed });
-
-            bot.user.setActivity(message.guild.memberCount + " Membros! ❤")
