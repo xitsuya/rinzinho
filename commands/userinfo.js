26bf14b496cb7ef5f178695ebc0506a35481fc0e
Glitch (scarce-cocktail) <none>
2018-06-30 01:57:35 +0000

    🏡🌮 Checkpoint ./commands/userinfo.js:1323575/2011 ./commands/wasted.js:1323575/2719 ./commands/shen.js:1323575/17290 ./commands/setgame.js:1323575/960

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,3 +1,25 @@
+const Discord = require("discord.js")
+const moment = require('moment')
+exports.run = (bot, message, args) => {
+        
+let parts = message.content.split(' ');
+let guild = message.guild;
+let user = message.mentions.users.first();
+let xd = guild.channels.find("name", "rin-bot");
+let xp = guild.channels.find("name", "geral");
+var embed = new Discord.RichEmbed()
+.setAuthor('As Suas Informações:', message.author.avatarURL)
+.addField('Seu Username', message.author.username, true)
+.addField('Seu Apelido:', message.guild.members.get(message.author.id).displayName, true)
+.addField('ID', message.author.id, true)
+.addField('Status', message.author.presence.status, true)
+.setThumbnail(message.author.avatarURL)
+.setTimestamp()
+.addField('Dia em que entrou no servidor', moment(message.guild.member(message.author).joinedAt).format('LL'), true)
+.addField('Quando você criou a sua conta:', moment(message.author.createdAt).format('LL'), true)
+.addField('Seus Cargos:', message.guild.members.get(message.author.id).roles.array().join(' | '))
+.setColor("#91489a")
+.setFooter("Comando Executado Por: " + message.author.tag, message.author.avatarURL)
 if (message.mentions.users.size < 1) return xd.send({ embed }).then(message => {
     var embed = new Discord.RichEmbed()
     .setAuthor("Rin-Bot", "https://cdn.discordapp.com/avatars/366922505480568852/8bc60c31c633300d19976f5a353784a6.png?size=2048")
@@ -42,3 +64,20 @@ if (message.mentions.users.size < 1) return xd.send({ embed }).then(message => {
     message.channel.send({embed}).then(message => message.delete(5000))
 
 var embed = new Discord.RichEmbed()
+    .setAuthor('A Info do Usuário Mencionado:', user.avatarURL)
+    .addField('Username do Membro', user.username, true)
+    .addField('Apelido do Membro:', message.guild.members.get(user.id).displayName, true)
+    .addField('ID', user.id, true)
+    .addField('Status', user.presence.status, true)
+    .setThumbnail(user.avatarURL)
+    .setTimestamp()
+    .addField('Dia em que entrou no servidor', moment(message.guild.member(user).joinedAt).format('LL'), true)
+    .addField('Criou a conta em:', moment(user.createdAt).format('LL'), true)
+    .addField('Cargos do Membro:', message.guild.members.get(user.id).roles.array().join(' | '))
+    .setColor("#91489a")
+    .setFooter("Comando Executado Por: " + message.author.tag, message.author.avatarURL)
+    xd.send({ embed })
+
+   bot.user.setActivity(message.guild.memberCount + " Membros! ❤")
+
+}
---
+++ commands/wasted.js
@@ -0,0 +1,56 @@
+const Discord = require("discord.js")
+exports.run = (bot, message, args) => {
+        
+let randomNumber = Math.floor(Math.random() * 8);
+if (randomNumber === 1) {
+    var embed = new Discord.RichEmbed()
+    .setImage("https://images-ext-1.discordapp.net/external/5gWWY_xqzhR914VnkAnRl8FCEcE29F65C-R15ffTjqM/https/cdn.weeb.sh/images/B1VnoJFDZ.gif")
+    .setColor("#91489a")
+    .setDescription("<@"+message.author.id+"> *Got Wasted*")
+    message.channel.sendMessage({embed})
+} else if (randomNumber === 2) {
+    var embed = new Discord.RichEmbed()
+    .setImage("https://images-ext-1.discordapp.net/external/e8238C-iXxfFlnuzY5unlLwkTTODuS2hfQjic39k6yo/https/cdn.weeb.sh/images/B1qosktwb.gif")
+    .setColor("#91489a")
+    .setDescription("<@"+message.author.id+"> *Got Wasted*")
+    message.channel.sendMessage({embed})
+
+} else if (randomNumber === 3) {
+    var embed = new Discord.RichEmbed()
+    .setImage("https://images-ext-2.discordapp.net/external/WYQmIFpoxHZqvX_U-HiOMFDyXuOZvigYVNBsEKBxqlA/https/cdn.weeb.sh/images/r11as1tvZ.gif")
+    .setColor("#91489a")
+    .setDescription("<@"+message.author.id+"> *Got Wasted*")
+    message.channel.sendMessage({embed})
+} else if (randomNumber === 4) {
+  var embed = new Discord.RichEmbed()
+    .setImage("https://images-ext-1.discordapp.net/external/4nztNvTnX0WDlvW0n71rLjgLbQt-yHY-kD4nrBoPB0I/https/cdn.weeb.sh/images/HyXTiyKw-.gif")
+    .setColor("#91489a")
+    .setDescription("<@"+message.author.id+"> *Got Wasted*")
+    message.channel.sendMessage({embed})
+} else if (randomNumber === 5) {
+    var embed = new Discord.RichEmbed()
+    .setImage("https://media.giphy.com/media/3ov9k09GrGnxSQLmk8/giphy.gif")
+    .setColor("#91489a")
+    .setDescription("<@"+message.author.id+"> *Got Wasted*")
+    message.channel.sendMessage({embed})
+} else if (randomNumber === 6) {
+    var embed = new Discord.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/367450857039593475/367737802110271489/6RuKoez.gif")
+    .setColor("#91489a")
+    .setDescription("<@"+message.author.id+"> *Got Wasted*")
+    message.channel.send({embed})
+}  else if (randomNumber === 7) {
+    var embed = new Discord.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/367450857039593475/367740266989486090/tenor.gif")
+    .setColor("#91489a")
+    .setDescription("<@"+message.author.id+"> *Got Wasted*")
+    message.channel.send({embed})
+}else if (randomNumber === 8) {
+    var embed = new Discord.RichEmbed()
+    .setImage("https://media.giphy.com/media/l1J9Gj5Z2gldp4dMs/giphy.gif")
+    .setColor("#91489a")
+    .setDescription("<@"+message.author.id+"> *Got Wasted*")
+    message.channel.send({embed})
+   bot.user.setActivity(message.guild.memberCount + " Membros! ❤")
+}
+}
