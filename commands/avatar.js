23c2fe2a62b15f21de0b59c5ca04d8ca999d91db
Glitch (rin) <none>
2018-07-27 02:54:11 +0000

    ☂️📫 Checkpoint ./commands/avatar.js:1482440/2

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,4 @@
- const Discord = require("discord.js")
+   const Discord = require("discord.js")
 exports.run = (bot, message, args) => {
     bot.user.setActivity(message.guild.memberCount + " Membros! ❤")
     let parts = message.content.split(' ');
