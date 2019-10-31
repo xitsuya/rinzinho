fb57f5520ee098abb879a1603ab296acb7704721
Glitch (rin) <none>
2019-10-31 23:52:20 +0000

    🐒🐵 Checkpoint ./main.js:751456/9839 ./package.json:751456/1499

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const chalk = require ("chalk");
+const chalk = require("chalk");
 const fs = require("fs");
-const warning = chalk.keyword('red');
+const warning = chalk.keyword("red");
 const config = require("./config.json");
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 
 const app = express();
 app.get("/", (request, response) => {
@@ -16,11 +16,11 @@ app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
-   client.prefix = config.prefix;
+client.prefix = config.prefix;
 
 client.on("ready", () => {
-console.log("Im Online ~ ♥")
-client.user.setActivity("Meu prefixo é \"+\"")
+  console.log("Im Online ~ ♥");
+  client.user.setActivity('Meu prefixo é "+"');
 });
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
@@ -36,10 +36,13 @@ fs.readdir("./events/", (err, files) => {
 
 client.on("message", message => {
   if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
 
   // This is the best way to define args. Trust me.
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content
+    .slice(config.prefix.length)
+    .trim()
+    .split(/ +/g);
   const command = args.shift().toLowerCase();
 
   // The list of if/else is replaced with those simple 2 lines:
@@ -52,92 +55,116 @@ client.on("message", message => {
 });
 
 client.on("messageUpdate", (oldMessage, newMessage) => {
-  if(newMessage.author.bot) return;
-var embed = new Discord.RichEmbed()
-.setAuthor(newMessage.author.tag, newMessage.author.avatarURL)
-.setDescription("**📝 | " + newMessage.author.tag +" | Editou uma mensagem no canal "+ newMessage.channel + "**" + "\n" + "```" + "\n" + oldMessage.content + " ```" + "\n ```" + newMessage.content + "" + "```")
-.setTimestamp()
-.setFooter("ID: "+ newMessage.author.id)
-.setColor("#91489a")
-let canal = newMessage.guild.channels.find("name", "rin-log")
-if(!canal) return;
-canal.send({embed})
-
-client.user.setActivity(newMessage.guild.memberCount + " Minions! ~ ❤")
-})
-
-client.on("guildCreate", (guild)=> {
-  guild.fetchInvites().then(invite => {
-  console.log(warning("Ei eu entrei no servidor " + guild.name + " O codigo do convite é https//:discord.gg:/"+ invite.first().code));
-  
-  })
-});
-
-client.on("guildCreate", (guild)=> {
-  guild.fetchInvites().then(invite => {
-  console.log(warning("Ei eu entrei no servidor : " + guild.name));
-  
-});
-    
-})
-
-client.on("messageDelete", (message) =>{
-  if(message.author.bot) return;
+  if (newMessage.author.bot) return;
   var embed = new Discord.RichEmbed()
-  .setAuthor(message.author.tag, message.author.avatarURL)
-  .setDescription("** 🗑 | " + message.author.tag + " | Apagou uma mensagem no canal "+ message.channel + "**" + "\n" + "```"+message.content+"```")
-  .setTimestamp()
-  .setFooter("ID: "+ message.author.id)
-  .setColor("#91489a")
-  let canal = message.guild.channels.find("name", "rin-log")
-  if(!canal) return;
-  canal.send({embed})
+    .setAuthor(newMessage.author.tag, newMessage.author.avatarURL)
+    .setDescription(
+      "**📝 | " +
+        newMessage.author.tag +
+        " | Editou uma mensagem no canal " +
+        newMessage.channel +
+        "**" +
+        "\n" +
+        "```" +
+        "\n" +
+        oldMessage.content +
+        " ```" +
+        "\n ```" +
+        newMessage.content +
+        "" +
+        "```"
+    )
+    .setTimestamp()
+    .setFooter("ID: " + newMessage.author.id)
+    .setColor("#91489a");
+  let canal = newMessage.guild.channels.find("name", "rin-log");
+  if (!canal) return;
+  canal.send({ embed });
 
-  client.user.setActivity(message.guild.memberCount + " Minions! ~ ❤")
-})
-client.on('userUpdate',(oUser, nUser) => {
-    client.guilds.forEach((guild) => {
-      if (guild.members.has(nUser.id) && oUser.avatarURL !== nUser.avatarURL) {
-        var embed = new Discord.RichEmbed()
+  client.user.setActivity(newMessage.guild.memberCount + " Minions! ~ ❤");
+});
+
+client.on("guildCreate", guild => {
+  guild.fetchInvites().then(invite => {
+    console.log(
+      warning(
+        "Ei eu entrei no servidor " +
+          guild.name +
+          " O codigo do convite é https//:discord.gg:/" +
+          invite.first().code
+      )
+    );
+  });
+});
+
+client.on("guildCreate", guild => {
+  guild.fetchInvites().then(invite => {
+    console.log(warning("Ei eu entrei no servidor : " + guild.name));
+  });
+});
+
+client.on("messageDelete", message => {
+  if (message.author.bot) return;
+  var embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag, message.author.avatarURL)
+    .setDescription(
+      "** 🗑 | " +
+        message.author.tag +
+        " | Apagou uma mensagem no canal " +
+        message.channel +
+        "**" +
+        "\n" +
+        "```" +
+        message.content +
+        "```"
+    )
+    .setTimestamp()
+    .setFooter("ID: " + message.author.id)
+    .setColor("#91489a");
+  let canal = message.guild.channels.find("name", "rin-log");
+  if (!canal) return;
+  canal.send({ embed });
+
+  client.user.setActivity(message.guild.memberCount + " Minions! ~ ❤");
+});
+client.on("userUpdate", (oUser, nUser) => {
+  client.guilds.forEach(guild => {
+    if (guild.members.has(nUser.id) && oUser.avatarURL !== nUser.avatarURL) {
+      var embed = new Discord.RichEmbed()
         .setColor("#91489a")
-        .setFooter("ID: " + nUser.id ) 
+        .setFooter("ID: " + nUser.id)
         .setTimestamp()
         .setAuthor(nUser.tag, oUser.avatarURL)
         .setDescription(":frame_photo:" + nUser + " **alterou o avatar**")
-        .setThumbnail(nUser.avatarURL)
-        let canal = guild.channels.find("name", "rin-log")
-        if(!canal) return;
-        canal.send({embed});
-      
-      }
-      if (guild.members.has(nUser.id) && oUser.username !== nUser.username) {
-        var embed = new Discord.RichEmbed()
+        .setThumbnail(nUser.avatarURL);
+      let canal = guild.channels.find("name", "rin-log");
+      if (!canal) return;
+      canal.send({ embed });
+    }
+    if (guild.members.has(nUser.id) && oUser.username !== nUser.username) {
+      var embed = new Discord.RichEmbed()
         .setColor("#91489a")
-        .setFooter("ID: " + nUser.id )
+        .setFooter("ID: " + nUser.id)
         .setTimestamp()
         .setAuthor(oUser.tag + " alterou o nome de usuário", nUser.avatarURL)
-        .setDescription("```"+ oUser.username + "```\n```"+nUser.username+"```")
-        .setThumbnail(nUser.avatarURL)
-        let canal = guild.channels.find("name", "rin-log")
-        if(!canal) return;
-        canal.send({embed});
-        
-      }
-    
-    });
+        .setDescription(
+          "```" + oUser.username + "```\n```" + nUser.username + "```"
+        )
+        .setThumbnail(nUser.avatarURL);
+      let canal = guild.channels.find("name", "rin-log");
+      if (!canal) return;
+      canal.send({ embed });
+    }
   });
+});
 
 client.on("message", message => {
-
-if(message.content.includes("https://discord")){
-  if(message.content === "https://discord.gg/RUc4vYz") return;
-  message.delete()
-  }else  if(message.channel.id === "396139789478395904"){
-  message.delete()
-}
-})
-
-
-
+  if (message.content.includes("https://discord")) {
+    if (message.content === "https://discord.gg/RUc4vYz") return;
+    message.delete();
+  } else if (message.channel.id === "396139789478395904") {
+    message.delete();
+  }
+});
 
 client.login(config.token);
